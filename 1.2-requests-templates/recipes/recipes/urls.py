7e6f8e37074740edba2f5aa3ea2136ec6348ef83
Max <alkansobaka@gmail.com>
2025-02-10 20:13:32 +0300

    Inital commit

--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 
 from django.urls import path
+from calculator.views import calculate_view
+
 
 urlpatterns = [
+    path('<dish_get>/', calculate_view, name='calculate')
     # здесь зарегистрируйте вашу view-функцию
 ]
